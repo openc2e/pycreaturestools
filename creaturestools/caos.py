8ef7dbf898b01bf72593cc731e2b70885713afc0
Michael Maltese <michaeljosephmaltese@gmail.com>
2022-01-01 20:09:38 -0800

    creaturestools.caos.format_c1_caos

---
+++ creaturestools/caos.py
@@ -0,0 +1,25 @@
+def format_c1_caos(script):
+    commands = script.split(",")
+    ret = ""
+    indent = 0
+    for command in commands:
+        word = command.strip().split(" ")[0].lower()
+        if word in ("else", "endi", "endm", "next", "repe", "retn", "untl"):
+            indent -= 1
+            # TODO: error if we went below 0?
+        if word == "subr":
+            ret += "\n"
+        ret += "  " * indent + command.strip() + "\n"
+        if word in (
+            "doif",
+            "else",
+            "enum",
+            "iscr",
+            "loop",
+            "reps",
+            "rscr",
+            "scrp",
+            "subr",
+        ):
+            indent += 1
+    return ret
