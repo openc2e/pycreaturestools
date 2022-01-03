cc1536a20e201afcf64abbd012fb5b1fc9b8662a
Michael Maltese <michaeljosephmaltese@gmail.com>
2022-01-02 18:41:24 -0800

    praydumper: learn how to automatically decompile sprites

--- creaturestools/bin/praydumper.py
+++ creaturestools/bin/praydumper.py
@@ -1,11 +1,27 @@
+import argparse
 import os
 import sys
 
 from creaturestools.pray import *
+from creaturestools.sprites import *
+
+
+def _replace_ext(fname, ext):
+    return os.path.splitext(fname)[0] + ext
 
 
 def main():
-    input_filename = sys.argv[1]
+    opts = argparse.ArgumentParser()
+    opts.add_argument("file")
+    opts.add_argument(
+        "--convert-images",
+        action="store_true",
+        help="Convert sprites into .png files",
+    )
+    args = opts.parse_args()
+
+    input_filename = args.file
+    convert_images = args.convert_images
     filename_root = os.path.basename(os.path.splitext(input_filename)[0])
     output_directory = filename_root
 
@@ -15,6 +31,22 @@ def main():
         pass
 
     def filenamefilter(relative_filename, data):
+        ext = os.path.splitext(relative_filename.lower())[1]
+        if convert_images and ext in (".s16", ".c16", ".blk"):
+            data = io.BytesIO(data)
+            if ext == ".s16":
+                image = stitch_to_sheet(read_s16_file(data))
+            elif ext == ".c16":
+                image = stitch_to_sheet(read_c16_file(data))
+            elif ext == ".blk":
+                image = read_blk_file(data)
+
+            relative_filename = _replace_ext(relative_filename, ".png")
+            output_filename = os.path.join(output_directory, relative_filename)
+            print(f"Writing {output_filename}")
+            image.save(output_filename)
+            return relative_filename
+
         output_filename = os.path.join(output_directory, relative_filename)
         print(f"Writing {output_filename}")
         with open(output_filename, "wb") as f:
